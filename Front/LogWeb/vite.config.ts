9fcd3cc7ebfb3e65eaec394c88077268a6789afa
Itay Laptev <itaylaptev@gmail.com>
2026-03-20 17:12:32 +0200

    admin/Superadmin fix

--- Front/LogWeb/vite.config.ts
+++ Front/LogWeb/vite.config.ts
@@ -1,16 +1,15 @@
-// JavaScript source code
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:5067",
-                changeOrigin: true,
-                secure: false
-            }
-        }
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:7176", //(http://localhost:5067, это и даёт 307)
+        changeOrigin: true,
+        secure: false
+      }
     }
+  }
 });
