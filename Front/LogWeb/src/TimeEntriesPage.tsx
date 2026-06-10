1084f0914cef473a17fd8f815dbcebcf869b1cc1
Itay Laptev <itaylaptev@gmail.com>
2026-06-10 18:44:34 +0300

    front login error

--- Front/LogWeb/src/TimeEntriesPage.tsx
+++ Front/LogWeb/src/TimeEntriesPage.tsx
@@ -19,6 +19,25 @@ export default function ProjectDetailsPage(){
             console.error(error);
         }
     }
+
+    async function handleStop(id: string) {
+        try{
+            await StopTimeEntry({note:"frontend stopped"}, id)
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async function handleDelete(id: string) {
+        try{
+            await DeleteEntry(id);
+            loadEntries();
+            //remove if loads twice
+        }catch(error){
+            console.error(error);
+        }
+    }
+
     useEffect(()=>{
         loadEntries();
     },[])
@@ -26,12 +45,16 @@ export default function ProjectDetailsPage(){
     return(
         <div style={{padding:20}}>
             <h1>Time Entries</h1>
+            {entries.length === 0 &&(<p>no time entries yet</p>)}
             {entries.map(entry =>
                 <div key={entry.Id}>
                     <p>Task: {entry.taskItemId}</p>
                     <p>Note: {entry.Note}</p>
                     <p>Start: {entry.StartedAtUtc}</p>
-                    <p>End: {entry.EndedAtUtc}</p>
+                    <p>End: {entry.EndedAtUtc ?? "Not done"}</p>
+                    <p>Duration: {entry.durationMinutes ?? "Running"}</p>
+                    {!entry.EndedAtUtc &&(<button onClick={() => handleStop(entry.Id)}>Stop</button>)}
+                    <button onClick={() => handleDelete(entry.Id)}>Delete entry</button>
                 </div>
             )}
         </div>
